c5bc2f4e24534097ead3f9a9723ba96370341707
Weili Yi <yi@weiliy.net>
2017-02-27 11:40:04 +0800

    update put the second screen rotate left

--- scripts/i3-script-autoxrandr.sh
+++ scripts/i3-script-autoxrandr.sh
@@ -6,6 +6,8 @@ extern=HDMI2
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
+    xrandr --output "$intern" --rotate normal
+    xrandr --output "$extern" --rotate left
     xrandr --output "$intern" --auto --left-of "$extern" --auto
 fi
 
